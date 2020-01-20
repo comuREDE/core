d1ca92b19c5823d20413e5cca9538a06b3b21bdc
Filipe Rimes <frimes@gmail.com>
2020-01-20 02:27:19 -0300

    Update install_lamp_comuREDE.sh

--- install_lamp_comuREDE.sh
+++ install_lamp_comuREDE.sh
@@ -30,6 +30,7 @@ sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gp
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php7.3.list;
 sudo apt update;
 sudo apt-get install php7.1 php7.1-cli php7.1-curl php7.1-json php7.1-opcache php7.1-common php7.1-readline -y;
+#sudo apt-get install php7.1 php7.1-cli php7.1-curl php7.1-json php7.1-opcache php7.1-common php7.1-readline php7.1-mysql -y;
 
 #SQL COMMANDS
 sudo mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION";
