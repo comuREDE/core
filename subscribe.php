79f285c4a65e657d3ade8834e452fd5fea14a914
Lucas Azevedo <lhs_azevedo@hotmail.com>
2024-11-20 17:48:26 -0300

    Sincroniza subscribe.php
    Sincroniza a última versão do script subscribe.php presente na VPS.
    Alterações observadas:
    - Suporte a mensagens MQTT com informações adicionais de pressão e
      vazão, usadas para monitoramento de água.
    - Logs diferenciados para mensagens de ÁGUA e LUZ.
    - Atualização do salvamento no banco para incluir pressão e vazão.

--- subscribe.php
+++ subscribe.php
@@ -24,18 +24,42 @@ function processaTopico($msg){
 	# $topic AGUA LUZ
 	#1 $msg (L/D)__12345-Y
 	#2 $msg (L/D)__12345789-XXXX
-	//echo "<br>",$msg," : ";
-	if(preg_match("/(L|D)__(\d{8})-(\d{1,2})/",$msg,$matches)){
+	#3 $msg (L/D)__12345789-XX_YY___ZZZ < X: sensor id | Y: pressao | Z: vazao
+
+	if(preg_match("/(L|D)__(\d{8})-(\d{1,2})_(P\d{1,4}|ND)___(F\d{1,4}|ND)/",$msg,$matches)){
+		//var_dump($matches);
 		$estado = $matches[1];
 		$cep = $matches[2];
 		$sensor = (int) $matches[3];
-		$arr = compact("estado","cep","sensor");
-		echo " -- Recebida MSG do MQTT --";
+		$pressao = (string) $matches[4];
+				if($pressao == 'ND'){
+						$pressao = NULL;
+				}else{
+					$pressao = (string) $matches[4];
+				}
+		$vazao = (string) $matches[5];
+				if ($vazao == 'ND') {
+					$vazao = NULL;
+					} else {
+					$vazao = (string) $matches[5];
+				}		
+		$arr = compact("estado","cep","sensor","pressao","vazao");
+		echo " -- Recebida MSG ÁGUA do MQTT --";
 		echo PHP_EOL;
-		echo " -- CEP: ".$cep." --";
+		echo " -- CEP: ".$cep." -- Pressão: ".$pressao." Vazão: ".$vazao;
 		echo PHP_EOL;
 		return $arr;
-	} 
+	} elseif(preg_match("/(L|D)__(\d{8})-(\d{1,2})/", $msg, $matches)){
+		$estado = $matches[1];
+		$cep = $matches[2];
+		$sensor = (int) $matches[3];
+		$arr = compact("estado", "cep", "sensor");
+		echo " -- Recebida MSG LUZ do MQTT --";
+		echo PHP_EOL;
+		echo " -- CEP: " . $cep;
+		echo PHP_EOL;
+		return $arr;
+	}
 	return null;
 }
 
@@ -54,8 +78,7 @@ function procmsg_agua($topic,$msg){
     $data = date("Y-m-d H:i:s");
 	$dados = processaTopico($msg);#"estado","cep","sensor"
 	extract($dados);#$estado $cep $sensor
-	$info = ['dia_hora'=>$data,'estado'=>$estado,'cep'=>$cep,'sensor'=>$sensor,];              
-
+	$info = ['dia_hora'=>$data,'estado'=>$estado,'cep'=>$cep,'sensor'=>$sensor,'pressao' => $pressao, 'vazao' => $vazao];              
 	$res = (new Model())->setTable('sensores_agua')->save($info);
 
 }
